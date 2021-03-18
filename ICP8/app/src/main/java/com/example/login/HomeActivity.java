2ecf4be026b8c092503cb4d9437847525f77abe9
harithaandem <harithaandem16@gmail.com>
2021-03-17 23:00:02 -0500

    ICP8

--- ICP8/app/src/main/java/com/example/login/HomeActivity.java
+++ ICP8/app/src/main/java/com/example/login/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
@@ -10,5 +12,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button btn = (Button) findViewById(R.id.logout_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myLogout();
+            }
+        });
+    }
+    public void myLogout()//when user clicks logout button
+    {
+        Intent redirect = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(redirect);
     }
 }
